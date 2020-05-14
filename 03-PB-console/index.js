4947589566e080df087dc8c4427b26befa2bdb41
Pedro <pedrojr_dantas@hotmail.com>
2020-05-14 12:00:23 +0200

    pedro

---
+++ 03-PB-console/index.js
@@ -0,0 +1,38 @@
+let text = 'This is an exercise in console logging';
+console.log(text);
+
+let myVariable = 'This is the value.';
+console.log(myVariable);
+
+let FirstName = 'Pedro';
+let SecondName = 'Dantas'
+console.log(FirstName, SecondName);
+
+let Fname = 'Amador';
+
+let Sname = 'Mato';
+
+let age = '33 anos';
+
+let job = 'Pintor';
+
+let city = 'Catanduvas';
+
+console.log ( Fname, Sname, city, age, job);
+
+
+Fname = 'Pedrinho';
+
+Sname = 'Matador';
+
+age = '33 anos';
+
+job = 'Matador';
+
+city = 'Matolandia';
+
+console.log( 'this is ' +  Fname, Sname, 'he is from ' + city);
+
+
+
+
