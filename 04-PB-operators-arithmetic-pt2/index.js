1e9d370a5ba444d276c09a75b0d657c00d5c8d64
Pedro <pedrojr_dantas@hotmail.com>
2020-05-17 20:45:34 +0200

    pedro

---
+++ 04-PB-operators-arithmetic-pt2/index.js
@@ -0,0 +1,71 @@
+'use strict';
+
+//Q1
+let soldTickets = 6450;
+/* console.log(soldTickets); */
+const Ticket = 15;
+console.log(soldTickets/Ticket + ' tickets were sold last nigth.');
+
+//Q2
+let WeekyIncome = 500;
+let year = 12;
+console.log(WeekyIncome * year + ' is the amount of money Sylvia makes per year.')
+
+//Q3
+let result = (17*100/30);
+console.log(result + '% is the percentage of 17/30.');
+//Q4
+let a = 4.75;
+let P = 4*a;
+console.log('The perimeter of the square is: ' + P+'cm.');
+
+//Q5
+let c = 5;
+let b = 6;
+let d = 7;
+
+let S = c + b + d;
+console.log('The perimeter of the triangle is: '+ S + 'cm.');
+
+//Q6
+let As = 5*5;
+console.log('The area of the square is : ' + As + 'cm².');
+
+//Q7
+let Area = Math.sqrt(S*(S-c)*(S-b)*(S-d));
+console.log(Area + 'cm² is the area of the triangle.');
+
+//Q8
+let Volume = 9*9*9;
+console.log(Volume + ' cm3');
+
+//Q9
+let billone = 22.35 + (22.35*10/100);
+console.log(billone + '$ 1-bill+tip');
+
+let billtwo = 26.67 + (22.35*10/100);
+console.log(billtwo + '$ 2-bill+tip');
+
+let billthree = 35.92 + (22.35*10/100);
+console.log(billthree + '$ 3-bill+tip');
+
+//Q10
+let NoemyAverageHours = (8 + 6 + 5 + 9 + 8 + 2 + 1 + 8.5 + 7 + 4)/10;
+console.log(NoemyAverageHours + ' hours average per day.');
+
+//Q11
+let x = 6*85-(75 + 70 + 85 + 90 + 100);
+console.log(x + ' is the score in the sixth test.');
+
+//Q12
+let AverageNeeded = (80 * 9)-(78 * 8);
+console.log('James needs a minimum of '+ AverageNeeded + '% to get an 80% average.');
+
+
+
+
+
+
+
+
+
