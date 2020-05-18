139d85adb778c0a7ea3f7be9ac4359b010c1eab8
Pedro <pedrojr_dantas@hotmail.com>
2020-05-18 11:22:09 +0200

    pedro ex-06

---
+++ 06-PB-ternary-exercises/index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+
+console.log('1.');
+let isDog;
+if (true) {
+    isDog = 'pat,pat';
+} else {
+    isDog = 'find me a dog to pat!';
+}
+console.log(isDog);
+
+
+console.log('2.');
+let speedlimit = 40;
+let speedCheck;
+
+if (speedlimit < 50) {
+    speedCheck = 'You are going too fast!';
+} else {
+    speedCheck = 'You are driving below the speed limit, Oma.';
+}
+console.log(speedCheck);
+
+console.log('3.');
+let age = 33;
+if (age < 16) {
+    age = 'Serve butter beer.';
+} else {
+    age = 'Serve beer.';
+}
+console.log(age);
+
+console.log('4.');
+let isStudent;
+if (true) {
+    isStudent = 'Ticket costs €5,00.';
+} else {
+    isStudent = 'Ticket costs €12,00.';
+}
+console.log(isStudent);
+let cake = true;
+console.log('5.');
+let okMarie;
+if (cake) {
+    okMarie = 'Let them eat cake.';
+} else {
+    okMarie = 'Oh, bother.'
+}
+console.log(okMarie);
