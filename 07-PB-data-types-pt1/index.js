a7fa78de4bc07740c5914818213ad8293b5a841b
Pedro <pedrojr_dantas@hotmail.com>
2020-05-18 14:51:08 +0200

    pedro

--- 07-PB-data-types-pt1/index.js
+++ 07-PB-data-types-pt1/index.js
@@ -1,4 +1,45 @@
 'use strict';
 
+console.log('1.');
 let myName = 'pedrinho';
 console.log(myName);
+
+console.log('2.');
+console.log(myName[1]+ ' is the 2nd character.');
+console.log(myName[5]+ ' is the 6th character.');
+
+console.log('3.');
+let name = 'Joazinho';
+console.log(name);
+
+console.log('4.');
+let age = 33;
+console.log(age);
+
+console.log('5.');
+let year = 2020;
+console.log(year);
+
+console.log('6.');
+console.log(name + ' is ' + age + ' in ' + year + '.');
+
+console.log('7.');
+let isMarried = true;
+console.log(isMarried);
+
+console.log('8.');
+console.log(name + ' is married:'+ [isMarried] + '.'); 
+
+console.log('9.');
+let programmingLanguage = 'JavaScript';
+console.log(programmingLanguage + ' is fun:'+ isMarried + '.')
+
+console.log('10.');
+console.log(programmingLanguage[0]);
+console.log(programmingLanguage[4]);
+
+console.log('11.');
+console.log(programmingLanguage.length + ' is tge length.');
+
+
+
