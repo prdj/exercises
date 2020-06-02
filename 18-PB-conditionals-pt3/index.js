bad3ab1c8423e20d4826ff1e3dc0a9755c748850
Pedro <pedrojr_dantas@hotmail.com>
2020-06-02 08:24:34 +0200

    up

--- 18-PB-conditionals-pt3/index.js
+++ 18-PB-conditionals-pt3/index.js
@@ -6,20 +6,22 @@ let maryTeam = (10 + 120 + 120) / 3;
 
 let winner = null;
 let score = null;
-
+let sentence = null;
 if (alezaTeam > lisTeam && alezaTeam > maryTeam) {
     winner = 'Aleeza\'s';
     score = alezaTeam;
-    console.log(`${winner} team scored average of ${score} points and is the winner.Congratulations to all the team members!`);
+    sentence = (`${winner} team scored average of ${score} points and is the winner.Congratulations to all the team members!`);
 
 } else if (lisTeam > alezaTeam && lisTeam > maryTeam) {
     winner = 'Lis\'s';
     score = lisTeam;
-    console.log(`${winner} team scored average of ${score} points and is the winner.Congratulations to all the team members!`);
+    sentence = (`${winner} team scored average of ${score} points and is the winner.Congratulations to all the team members!`);
 } else if (maryTeam > lisTeam && maryTeam > alezaTeam) {
     winner = 'Mary\'s';
     score = maryTeam;
-    console.log(`${winner} team scored average of ${score} points and is the winner.Congratulations to all the team members!`);
+    sentence = (`${winner} team scored average of ${score} points and is the winner.Congratulations to all the team members!`);
 } else {
-    console.log('We have a Draw in this game.');
+   sentence = ('We have a Draw in this game.');
 }
+
+console.log(sentence);
---
+++ 19-PB-conditionals-fizzbuzz-simple/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let trY = 11;
+if (trY % 3 === 0 && trY % 5 === 0){
+    console.log('FizzBuzz');
+}  else if (trY % 3 === 0) {
+    console.log('Fizz');
+}  else if (trY % 5 === 0) {
+    console.log('Buzz');
+} else {
+    console.log(trY);
+}
