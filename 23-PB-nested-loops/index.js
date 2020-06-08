f000030a3ecd3ab60a947cf5baf14d6d60f54354
Pedro <pedrojr_dantas@hotmail.com>
2020-06-08 08:29:00 +0200

    up

--- 23-PB-nested-loops/index.js
+++ 23-PB-nested-loops/index.js
@@ -1,15 +1,27 @@
 'use strict';
-
+/* 
 let space = "";
-for(let i=10; i >= 1; i--){
-  space = "";
-  for(let j=i; j < 10; j++){
-    space += " ";
-  }
-  for(let k=0; k < (i*2)-1; k++){
-    space += "*";
-  }
-  console.log(space);
-}  
+for (let i = 10; i >= 1; i--) {
+    space = "";
+    for (let j = i; j < 10; j++) {
+        space += " ";
+    }
+    for (let k = 0; k < (i * 2) - 1; k++) {
+        space += "*";
+    }
+    console.log(space);
+} */
 
 // You only need to change two things ate the first for.
+console.log('2.')
+
+for (let i = 1; i <= 10; i++) {
+    let space = "";
+    for (let j = i; j < 10; j++) {
+        space += "   ";
+    }
+    for (let k = 10; k < (i * 2) - 1; k++) {
+        space = space + " * ";
+        }
+    console.log(space);
+}
---
+++ 24-PB-function-declaring/index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+console.log('Ex-1')
+
+function exOne(a, b) {
+    console.log(a * b);
+}
+exOne(0, 1);
+
+console.log('Ex-2')
+
+const exTwo = function (a, b) {
+    console.log(a * b);
+}
+exTwo(2, 3);
+
+console.log('Ex-3')
+
+const exThree = (a, b) => {
+    console.log(a * b);
+}
+
+exThree(1, 2);
+
+console.log('Ex-4')
+
+function exFourA(a, b) {
+    let remainder = a % b;
+    remainder === 0 ? console.log(`0`) : console.log(`Remainder is ${remainder}`);
+}
+exFourA(30, 5);
+
+const exFourB = function (a, b) {
+    let remainder = a % b;
+    remainder === 0 ? console.log(`0`) : console.log(`Remainder is ${remainder}`);
+}
+exFourB(3, 5);
+
+const exFourC = (a, b) => {
+    let remainder = a % b;
+    remainder === 0 ? console.log(`0`) : console.log(`Remainder is ${remainder}`);
+}
+exFourC(19, 5);
